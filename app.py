58bd8e2d47007c6e226819585727e2a83b2c648c
Vipshal WG <vipshalaz123@gmail.com>
2023-05-29 16:11:49 +0530

    Upapp

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ db_config = {
     'database': 'mydatabase',  # Name of the database
 }
 
-# Connect to the EdgeDB instance
-conn = edgedb.connect(**db_config)
+# Create an async connection pool
+pool = edgedb.create_async_pool(**db_config)
 
 
 # Route for the dashboard page
@@ -24,47 +24,48 @@ def dashboard():
 
 # Route for adding a customer
 @app.route('/add_customer', methods=['POST'])
-def add_customer():
+async def add_customer():
     if request.method == 'POST':
         customer_name = request.form['customerName']
         customer_email_or_phone = request.form['customerEmail']
         customer_address = request.form['customerAddress']
 
-        # Check if the email or phone already exists in the database
-        query = """
-            SELECT (
-                SELECT count(customers)
-                FILTER customers.EmailorPhone = <str>$email_or_phone
-            ) > 0
-        """
-        exists = conn.query_single(query, email_or_phone=customer_email_or_phone)
+        async with pool.acquire() as conn:
+            # Check if the email or phone already exists in the database
+            query = """
+                SELECT (
+                    SELECT count(customers)
+                    FILTER customers.EmailorPhone = <str>$email_or_phone
+                ) > 0
+            """
+            exists = await conn.query_single(query, email_or_phone=customer_email_or_phone)
 
-        if exists:
-            flash('Customer with the same email or phone already exists')
-            return redirect(url_for('dashboard'))
+            if exists:
+                flash('Customer with the same email or phone already exists')
+                return redirect(url_for('dashboard'))
 
-        # Prepare the EdgeQL query to insert customer data
-        query = """
-            INSERT customers {
-                BillNumber := <str>$bill_number,
-                InstagramID := <str>$email_or_phone,
-                Address := <str>$address
+            # Prepare the EdgeQL query to insert customer data
+            query = """
+                INSERT customers {
+                    BillNumber := <str>$bill_number,
+                    InstagramID := <str>$email_or_phone,
+                    Address := <str>$address
+                }
+            """
+            values = {
+                'bill_number': customer_name,
+                'email_or_phone': customer_email_or_phone,
+                'address': customer_address
             }
-        """
-        values = {
-            'bill_number': customer_name,
-            'email_or_phone': customer_email_or_phone,
-            'address': customer_address
-        }
 
-        try:
-            # Execute the query
-            conn.execute(query, values)
-            conn.commit()
-            flash('Customer added successfully')
-        except Exception as e:
-            conn.rollback()
-            flash('Error adding customer: {}'.format(str(e)))
+            try:
+                # Execute the query
+                await conn.execute(query, values)
+                await conn.commit()
+                flash('Customer added successfully')
+            except Exception as e:
+                await conn.rollback()
+                flash('Error adding customer: {}'.format(str(e)))
 
         return redirect(url_for('dashboard'))
 
